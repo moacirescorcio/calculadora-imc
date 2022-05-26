a95860900ff49eb21122ebe7f3c2a986b9c798aa
Moacir <moacirescorcio@gmail.com>
2022-05-26 21:21:29 +0000

    mudanças js e html

--- comportamento.js
+++ comportamento.js
@@ -2,18 +2,20 @@ function app(){
     console.log('App iniciada')
 
     // conectar com o botão no html
-    const calcular = document.getElementById('calcular')
+    const btn_calcular = document.getElementById('#btn_calcular')
     const peso = document.getElementById('peso')
 
     // associar um comportamento ao botão
     // calcular.onclick = clicou_no_botao () => {alert('Clicou!')}
 
-    calcular.onclick = () => {
-        const peso = peso.value
-    }
+    // btn_calcular.onclick = () => {
+        // const peso = peso.value
+        // alert('Clicou!')
+    // }
+    btn_calcular.onclick = clicou_no_botao
 }
 
-// function clicou_no_botao(){
-//     alert('Clicou!')
-// }
+function clicou_no_botao(){
+    alert('Clicou!')
+}
 app()
