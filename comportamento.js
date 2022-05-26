6c7ce9945d40220f7ce40bbab84cfff119d40b2a
Moacir <moacirescorcio@gmail.com>
2022-05-26 22:56:59 +0000

    função calcular

--- comportamento.js
+++ comportamento.js
@@ -2,20 +2,44 @@ function app(){
     console.log('App iniciada')
 
     // conectar com o botão no html
-    const btn_calcular = document.getElementById('#btn_calcular')
-    const peso = document.getElementById('peso')
+    const btn_calcular = document.getElementById('btn_calcular')
+    const input_peso = document.getElementById('input_peso')
+    const input_altura = document.getElementById('input_altura')
+    const input_nome =document.getElementById('input_nome')
+
+    const show_imc = document.getElementById('show_imc')
+    const show_nome = document.getElementById('show_nome')
+    const show_resultado = document.getElementById('show_resultado')
 
     // associar um comportamento ao botão
-    // calcular.onclick = clicou_no_botao () => {alert('Clicou!')}
+    btn_calcular.onclick = () => {
+        const peso = Number(input_peso.value)
+        const altura = Number(input_altura.value)
+        const nome = input_nome.value
+        
 
-    // btn_calcular.onclick = () => {
-        // const peso = peso.value
-        // alert('Clicou!')
-    // }
-    btn_calcular.onclick = clicou_no_botao
+        const imc = peso / (altura + altura)
+        const resultado = classificar_imc(imc)
+
+        show_nome.innerText = 'Nome: ' + nome
+        show_imc.innerText = 'IMC: ' + imc.toFixed(2)
+        show_resultado.innerText = 'Resultado: ' +  resultado
+
+        
+
+    }
+
+    
 }
 
-function clicou_no_botao(){
-    alert('Clicou!')
+function classificar_imc(imc){
+    if (imc < 18.9){
+        return 'Abaixo do Peso'
+    }else if (imc > 24.9){
+        return 'Sobrepeso'
+    }else{
+        return 'Peso normal'
+    }
 }
+
 app()
