7b64af3ee5c33b1472b78c3726a9e7a300f84d9b
Moacir <moacirescorcio@gmail.com>
2022-05-27 00:37:34 +0000

    parte esquerda concluída

--- comportamento.js
+++ comportamento.js
@@ -10,6 +10,13 @@ function app(){
     const show_imc = document.getElementById('show_imc')
     const show_nome = document.getElementById('show_nome')
     const show_resultado = document.getElementById('show_resultado')
+    const show_sexo = document.getElementById('show_sexo')
+
+    const btn_sexo_m = document.getElementById('sexo_m')
+    const btn_sexo_f = document.getElementById('sexo_f')
+
+    let sexo 
+
 
     // associar um comportamento ao botão
     btn_calcular.onclick = () => {
@@ -24,21 +31,42 @@ function app(){
         show_nome.innerText = 'Nome: ' + nome
         show_imc.innerText = 'IMC: ' + imc.toFixed(2)
         show_resultado.innerText = 'Resultado: ' +  resultado
-
-        
-
+        show_sexo.innerText = 'Sexo: ' + sexo
     }
 
-    
+    btn_sexo_m.onclick = () => {
+        // alert('Masculino')
+        sexo = 'Masculino'
+        btn_sexo_m.classList.add('selecionado')
+        btn_sexo_f.classList.remove('selecionado')
+    }
+
+    btn_sexo_f.onclick = () => {
+        // alert('Feminino')
+        sexo = 'Feminino'
+        btn_sexo_f.classList.add('selecionado')
+        btn_sexo_m.classList.remove('selecionado')
+    }
 }
 
 function classificar_imc(imc){
-    if (imc < 18.9){
-        return 'Abaixo do Peso'
-    }else if (imc > 24.9){
+    if (imc < 18.5){
+        return 'Peso Baixo'
+    }
+    else if(imc < 24.9){
+        return 'Peso Normal'
+    }
+    else if (imc < 29.9){
         return 'Sobrepeso'
-    }else{
-        return 'Peso normal'
+    }
+    else if (imc < 34.9){
+        return 'Obesidade 1'
+    }
+    else if (imc < 39.9){
+        return 'Obesidade 2'
+    }
+    else {
+        return 'Obesidade 3'
     }
 }
 
